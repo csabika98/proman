4b0f3d5ee13d259ccd433e5046ae9769951d6864
csabika98 <6ix9inegotti@naver.com>
2021-03-17 11:17:11 +0100

    fixed version

---
+++ data_manager.py
@@ -0,0 +1,15 @@
+from typing import List, Dict
+
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
+
+import database_common
+
+
+@database_common.connection_handler
+def get_accounts(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT *
+        FROM accounts"""
+    cursor.execute(query)
+    return cursor.fetchall()
